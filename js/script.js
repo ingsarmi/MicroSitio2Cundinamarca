fd85043bc0b950172348273399f85d1d4f20725a
Sergio Sarmiento <105184718+ingsarmi@users.noreply.github.com>
2023-09-18 11:05:10 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,124 @@
+
+const productos = [
+  {
+    titulo: "Marca personal y reputación en línea",
+    imagen: "img/marcapersonal.png",
+    descripcion: "El apostar por la creación de la marca personal, es una duda frecuente entre las personas que requieren promover sus productos, servicios, experiencias, colaboraciones, conocimiento o simplemente crear comunidades, de esa manera se genera confianza para conectar con la audiencia dejando una huella, posicionándose y generando recordación en el mercado o comunidad. Contrarrestando así la desconfianza en el mercado competitivo de hoy haciendo uso de las redes sociales. En el curso de marca Personal y Reputación en línea, se abordan conceptos básicos para la creación de una marca, las estrategias y habilidades que se necesitan como factor diferenciador sobre la competencia directa, para crear una marca personal y tener una buena reputación en línea. Para ello, se revisarán, los beneficios, el impacto, el marketing personal y la importancia de las redes sociales, en la marca personal o “personal branding”, así como la identidad digital y reputación en línea que es el reconocimiento que hacen los seguidores o potenciales compradores de la marca que hemos creado.".replace(/\./g, ".<br><br>"),
+    enlace: "#"
+  },
+  {
+    titulo: "COACHING",
+    imagen: "img/coaching.png",
+    descripcion: "El coaching está presente en todos los aspectos del ser y tiene que ver con la manera en que funciona todo su sistema de coherencia (pensar, sentir, decir y hacer). Al revisarse honestamente y sin juicios, podrá explorar de qué manera le sirven o limitan sus pensamientos y acciones para crear más opciones que enriquezcan la vida, la profesión y el entorno laboral, social y de negocios. Este curso es una inmersión en el mundo del coaching, aprendiendo gran variedad de dinámicas y herramientas de calidad que propiciarán el cambio y el desarrollo personal, profesional y organizacional. El coaching ha surgido apoyándose en las neurociencias y es aplicable en un amplio campo, en el que se llevan a cabo una serie de acciones para acompañar a otras personas a conseguir sus objetivos, potenciar sus habilidades y aportar recursos para superar sus limitaciones, lograr objetivos de alto estándar y mejorar su desempeño o reenfocar sus propósitos.  ".replace(/\./g, ".<br><br>"),
+    enlace: "#"
+  },
+  {
+    titulo: "Creatividad e innovación",
+    imagen: "img/creatividad.png",
+    descripcion: "Al realizar este curso, usted conocerá técnicas prácticas para diseñar un proyecto creativo e innovador aplicable en un entorno organizacional y/o social. El proceso inicia identificando aspectos básicos para la generación y concreción de ideas innovadoras basadas en la aplicación de la técnica “Design Thinking”. Posteriormente, se abordará la técnica SCAMPER, que brinda a los proyectos los elementos creativos necesarios para finalmente, construir una propuesta de valor única y diferencial, que tenga repercusión en el entorno. Esperamos que este curso satisfaga sus expectativas de aprendizaje y le sea de suma utilidad tanto en su contexto personal como laboral.".replace(/\./g, ".<br><br>"),
+    enlace: "	https://institucion.ucundinamarca.edu.co/course/view.php?id=255"
+  },
+  {
+    titulo: "Beginners English Course",
+    imagen: "img/beginners.png",
+    descripcion: "El Beginners English Course contribuye de manera práctica a que el estudiante no solo comprenda las ideas principales de exposiciones y textos sencillos producidos en lengua extranjera (inglés), sino a que elabore sus propios discursos aplicando los conocimientos que paulatinamente construye en cada experiencia de aprendizaje. Desde esta perspectiva, se potencia el desarrollo de las habilidades comunicativas en lengua extranjera (Listening, Reading, Writing and Speaking), mediante la realización de actividades aplicativas y contextualizadas propuestas en este curso y así alcanzar el nivel A1 según el Marco Común Europeo (MCE), ampliando de esta manera, su campo de acción profesional y personal, generando aprendizajes para la vida. Esperamos que este curso satisfaga sus expectativas de aprendizaje y le sea de suma utilidad tanto en su contexto personal como laboral.".replace(/\./g, ".<br><br>"),
+    enlace: "https://institucion.ucundinamarca.edu.co/course/view.php?id=264"
+  },
+  {
+    titulo: "Emprendimiento Personal y Empresarial",
+    imagen: "img/empredimientopersonal.png",
+    descripcion: "Con la realización de este curso, usted conocerá los fundamentos conceptuales y las herramientas de aplicación necesarias para gestionar proyectos de manera efectiva a nivel personal y empresarial. Este proceso iniciará con la identificación de los tipos de emprendimiento que se pueden dar en el entorno organizacional, relacionando las habilidades emprendedoras requeridas para la consolidación de un proyecto personal u organizacional. Finalmente, se abordará la metodología CANVAS, con la que se explorarán aspectos clave a tener en cuenta para estructurar un modelo de negocio innovador. Esperamos que este curso satisfaga sus expectativas de aprendizaje y le sea de suma utilidad tanto en su contexto personal como laboral.".replace(/\./g, ".<br><br>"),
+    enlace: "https://institucion.ucundinamarca.edu.co/course/view.php?id=256"
+  },
+  {
+    titulo: "Inteligencia situacional y emocional",
+    imagen: "img/inteligencia.png",
+    descripcion: "En el curso De la Administración por Objetivos a la Administración por Resultados, usted aprenderá cómo identificar el involucramiento de los colaboradores en una organización basada en la Administración por objetivos y de este modo, proyectar estrategias de trabajo que, definidas mancomunadamente, permitan establecer metas corporativas y/o colectivas que conduzcan al éxito de la organización. Además, se planteará la aplicación del modelo de Gestión Organizacional por Resultados que, al proyectarse hacia el alcance de los objetivos definidos en el primer modelo, permite establecer una ruta de resultados para beneficio personal, profesional y organizacional. Esperamos que este curso satisfaga sus expectativas de aprendizaje y le sea de suma utilidad tanto en su contexto personal como laboral. ".replace(/\./g, ".<br><br>"),
+    enlace: "https://institucion.ucundinamarca.edu.co/course/view.php?id=249"
+  },
+  {
+    titulo: "Análisis de datos",
+    imagen: "img/analisisdedatos.png",
+    descripcion: "En el curso <strong>Análisis de Datos</strong>, usted empleará técnicas y tecnologías de análisis de datos, las cuales son útiles en la toma de decisiones en una organización. De igual forma, usted reconocerá y evaluará el uso de la información por parte de grandes y pequeñas organizaciones en el siglo XXI, enfatizando en la importancia del análisis de datos como estrategia para la toma de decisiones acertadas. Finalmente, aprenderá cómo implementar técnicas de análisis de datos en su vida diaria, basadas en diferentes tecnologías, para que usted sea más eficiente en sus labores a nivel personal, profesional u organizacional. Esperamos que este curso satisfaga sus expectativas de aprendizaje y le sea de suma utilidad tanto en su contexto personal como laboral. ".replace(/\./g, ".<br><br>"),
+    enlace: "https://institucion.ucundinamarca.edu.co/course/view.php?id=258"
+  },
+  {
+    titulo: "Gestión del Cambio",
+    imagen: "img/gestion.png",
+    descripcion: "En este curso, usted aprenderá cómo gestionar el cambio a partir del liderazgo innovador en el entorno organizacional, identificando necesidades, formulando estrategias y monitoreando el proceso para hacer un seguimiento efectivo del cambio en cuestión. Esto, teniendo en cuenta que, asegurar la eficacia se ha convertido en una necesidad para mantenerse vigente y no fracasar. Por esto, cada empresa requiere de transformaciones y actualizaciones, no solo en procesos y procedimientos, sino también desde el quehacer de sus trabajadores y para lograrlo, es necesario tener en cuenta los aspectos necesarios para una correcta <strong>gestión del cambio</strong>. Confiamos en que este curso satisfaga sus expectativas de aprendizaje y le sea de suma utilidad tanto en su contexto personal como laboral.".replace(/\./g, ".<br><br>"),
+    enlace: "https://institucion.ucundinamarca.edu.co/course/view.php?id=263"
+  },
+  {
+    titulo: "Resolución de Conflictos",
+    imagen: "img/resolucion.png",
+    descripcion: "En el presente curso usted conocerá la forma de aplicar la resolución de conflictos ante cualquier desacuerdo en diferentes entornos, abriendo espacios para la convivencia basada en valores democráticos, la civilidad y la libertad. Este curso considera los elementos necesarios para lograr una resolución efectiva de situaciones que afectan la convivencia, mediante la implementación de <strong>Métodos de Resolución conflictos (MTC) </strong>, previstos a nivel nacional e internacional. Lo cuales, permiten controlar de manera asertiva diversas situaciones problémicas, al hacer uso de las habilidades comunicativas en una correcta mediación, pues, esto con seguridad facilitará que, sus relaciones con la comunidad en general se den en un ambiente de resolución y no violencia. Confiamos en que lograremos llenar sus expectativas con este curso y estamos seguros de que le será de suma utilidad tanto en su contexto personal como laboral. ".replace(/\./g, ".<br><br>"),
+    enlace: "https://institucion.ucundinamarca.edu.co/course/view.php?id=247"
+  },
+  {
+    titulo: "Estilos de Gerencia",
+    imagen: "img/estilos.png",
+    descripcion: "Con la realización de este curso y como parte del proceso de aprendizaje usted conocerá herramientas que le permitirán convertirse en un gran gerente, adaptando los estilos de gerencia al entorno organizacional en el que se encuentre, lo que le permitirá consolidar equipos y ser un líder estratégico de alto rendimiento. Los elementos trabajados en este curso le ayudarán a optimizar las relaciones interpersonales en el ambiente laboral, a través de los diferentes estilos de gerencia, utilizando la competitividad estratégica y el desarrollo humano como pilar fundamental para la solución de diversas problemáticas. Confiamos en que lograremos llenar sus expectativas con este curso y estamos seguros de que le será de suma utilidad tanto en su contexto personal como laboral.".replace(/\./g, ".<br><br>"),
+    enlace: "	https://institucion.ucundinamarca.edu.co/course/view.php?id=253"
+  },
+  {
+    titulo: "Pensamiento Lógico",
+    imagen: "img/pensamiento.png",
+    descripcion: "Con la realización de este curso y como parte del proceso, usted construirá conocimientos para generar soluciones rápidas, eficientes y metódicas a distintas situaciones problémicas o conflictos a través del uso del pensamiento lógico, basado en hechos y datos probables. Por medio del uso del pensamiento lógico, usted tendrá la oportunidad de desarrollar precisión, asertividad y exactitud, para poder entender su contexto y las relaciones presentes en él, abstraídos a partir de la observación, el análisis, la comparación, la clasificación, la experiencia, la imaginación y la creatividad. El pensamiento lógico es una herramienta que permite crear soluciones rápidas y eficientes a problemas de la vida diaria, abarcando desde lo más simple hasta lo más complejo. Confiamos en que este curso satisfaga sus expectativas de aprendizaje y le sea de suma utilidad tanto en su contexto personal como laboral.".replace(/\./g, ".<br><br>"),
+    enlace: "	https://institucion.ucundinamarca.edu.co/course/view.php?id=254"
+  },
+  {
+    titulo: "Análisis y Evaluación de Estrategias",
+    imagen: "img/analisisyevaluacion.png",
+    descripcion: "A través del curso de Análisis y Evaluación de Estrategias, usted podrá conocer elementos que le brindarán herramientas para convertirse en un estratega analítico que alcanza sus logros a corto, mediano y largo plazo, tanto en lo profesional como en lo personal. Hoy en día y debido a los nuevos retos que se requiere enfrentar en el mundo, es imperativo que usted tenga una perspectiva holística y sistémica de la formulación, análisis y evaluación de estrategias, que lo lleve no solo a mejorar continuamente su desempeño laboral o profesional, sino también, a planificar, gestionar, controlar los recursos, así como las metas personales y familiares. Es por ello, que este curso se centra en proporcionarle de manera práctica y aplicativa las diferentes herramientas que debe tener en cuenta para ser un exitoso líder de análisis y evaluación estratégica. Esperamos que este curso satisfaga sus expectativas de aprendizaje y le sea de suma utilidad tanto en su contexto personal como laboral.".replace(/\./g, ".<br><br>"),
+    enlace: "https://institucion.ucundinamarca.edu.co/course/view.php?id=262"
+  },
+  {
+    titulo: "Metodología de la Innovación- Desing Thinking y Lean Startup",
+    imagen: "img/metodologia.png",
+    descripcion: "El curso Metodologías de Innovación – Design Thinking y Lean Startup, le permitirá identificar la importancia de las metodologías innovadoras en la actualidad y reconocerlas como la alternativa adecuada para quien pretende romper paradigmas y transformar procesos tradicionales. Lo anterior, puede ser ampliamente aplicado en la vida cotidiana, pues modifica estructuras, procesos convencionales o variables del entorno, en el ámbito familiar, en lo social o en lo laboral a través de un pensamiento creativo, lo que trae como resultado nuevas experiencias. En este sentido, el curso le ofrecerá las herramientas necesarias para que usted esté en capacidad de crear ideas novedosas que le permitan fortalecer procesos y aportar un valor diferencial para obtener resultados sorprendentes ante diferentes problemáticas en un determinado escenario. Confiamos en que este curso satisfaga sus expectativas de aprendizaje y le sea de suma utilidad tanto en su contexto personal como laboral.".replace(/\./g, ".<br><br>"),
+    enlace: "https://institucion.ucundinamarca.edu.co/course/view.php?id=252"
+  },
+  {
+    titulo: "Agilismo",
+    imagen: "img/agilismo.png",
+    descripcion: "El curso <strong>Agilismo</strong>, ha sido planteado como un espacio para el desarrollo de aptitudes y actitudes agilistas en el campo organizacional. Por lo cual, se partirá de la identificación de necesidades de innovación y mejoramiento en el flujo de trabajo al interior de las organizaciones, los participantes realizarán un acercamiento a la metodología desde sus características, principios, estructura y por me medio de experiencias propias del entorno laboral, también desarrollarán habilidades que les permitirán mejorar la ejecución de cualquier tipo de tarea en el entorno personal. En este sentido, el curso brinda las herramientas necesarias para el desarrollo de la adaptabilidad, la planificación y la exitosa ejecución de tareas y proyectos, basándose en la anticipación de eventualidades y suprimiendo la procrastinación. Finalmente, el participante habrá desarrollado un plan estratégico agilista para la ejecución organizada de pequeñas tareas en procesos organizacionales. Confiamos en que este curso satisfaga sus expectativas de aprendizaje y le sea de suma utilidad tanto en su contexto personal como laboral.".replace(/\./g, ".<br><br>"),
+    enlace: "https://institucion.ucundinamarca.edu.co/course/view.php?id=259"
+  },
+  {
+    titulo: "Inteligencia situacional y emocional ",
+    imagen: "img/inteligencia.png",
+    descripcion: "El curso Inteligencia situacional y emocional pretende que el participante viva una experiencia de aprendizaje en torno a la implementación de técnicas y estrategias de control emocional que faciliten la adaptación eficiente del individuo al ambiente laboral, permitan la resolución de problemas de forma empática y aporten a su desarrollo humano y personal. A partir de diferentes estrategias de inteligencia emocional, la construcción de un ambiente de trabajo sustentado en valores contribuirá a su formación para la vida, facilitando el manejo de situaciones de tensión o conflicto, fomentando el liderazgo, el manejo del estrés, el desarrollo de actividades bajo presión, la toma de decisiones asertivas y la resolución de conflictos, aspectos estos, que contribuyen al desarrollo personal, profesional e institucional. Finalmente, el participante habrá alcanzado su resultado esperado de aprendizaje y sabrá gestionar sus emociones al interactuar con otros. Esperamos que este curso satisfaga sus expectativas de aprendizaje y le sea de suma utilidad tanto en su contexto personal como laboral.".replace(/\./g, ".<br><br>"),
+    enlace: "https://institucion.ucundinamarca.edu.co/course/view.php?id=249"
+  },
+  {
+    titulo: "Negociación efectiva",
+    imagen: "img/negociacion.png",
+    descripcion: "El curso Negociación efectiva brinda a los participantes herramientas y modelos de negociación para que enfrenten y resuelvan conflictos de forma efectiva, no solo de orden laboral, sino personal, a través de negociaciones exitosas. La clave fundamental que se pretende instituir en este espacio, es la importancia del cambio como estrategia de mejoramiento, de crecimiento y de innovación, por lo tanto, el cambio planificado y gestionado de manera efectiva y eficiente, será el pilar para que el participante enfrente las situaciones problémicas que se le presentan en la cotidianidad de una mejor manera.Confiamos en que este curso satisfaga sus expectativas de aprendizaje y le sea de suma utilidad tanto en su contexto personal como laboral.".replace(/\./g, ".<br><br>"),
+    enlace: "https://institucion.ucundinamarca.edu.co/course/view.php?id=248"
+  },
+  {
+    titulo: "Construcción y trabajo colaborativo",
+    imagen: "img/construccion.png",
+    descripcion: "El curso Construcción y trabajo colaborativo se plantea como una experiencia formativa con el fin de que el participante valore la importancia de las prácticas, actitudes y comportamientos que configuran la cultura organizacional y de qué manera influyen en el alcance de las metas y objetivos. Las organizaciones están llamadas a configurar relaciones entre sus colaboradores que, permeadas por los principios y fundamentos, inspiren actitudes colaborativas, cooperativas y solidarias que migrarán hacia otros contextos de actuación de los sujetos vinculados a ellas, el resultado esperado de aprendizaje de este curso responde a esta necesidad creando una metodología dirigida a la creación de una comunidad colaborativa de trabajo. Esperamos en que este curso satisfaga sus expectativas de aprendizaje y le sea de suma utilidad tanto en su contexto personal como laboral.".replace(/\./g, ".<br><br>"),
+    enlace: "https://institucion.ucundinamarca.edu.co/course/view.php?id=257"
+  }
+  // Agrega más productos aquí...
+];
+
+function mostrarProductos() {
+  const productosContainer = document.getElementById("productos-container");
+  
+  productos.forEach(producto => {
+    const productoHTML = `
+      <div class="producto">
+        <h2>${producto.titulo}</h2>
+        <img src="${producto.imagen}" alt="${producto.titulo}">
+        <p>${producto.descripcion}</p>
+        <a target="_blank" href="${producto.enlace}">Inscribirse</a>
+      </div>
+    `;
+    
+    productosContainer.innerHTML += productoHTML;
+  });
+}
+mostrarProductos(); 
