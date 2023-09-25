7f3eddb541b480a321f07abb16a1262e19933a7f
Sergio Sarmiento <105184718+ingsarmi@users.noreply.github.com>
2023-09-25 17:54:41 -0500

    se actualizaron algunos contenidos entre estos el logo y el ico

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 const productos = [
-  {
+  /* {
     titulo: "Marca personal y reputación en línea",
     imagen: "img/marcapersonal.png",
     descripcion: "El apostar por la creación de la marca personal, es una duda frecuente entre las personas que requieren promover sus productos, servicios, experiencias, colaboraciones, conocimiento o simplemente crear comunidades, de esa manera se genera confianza para conectar con la audiencia dejando una huella, posicionándose y generando recordación en el mercado o comunidad. Contrarrestando así la desconfianza en el mercado competitivo de hoy haciendo uso de las redes sociales. En el curso de marca Personal y Reputación en línea, se abordan conceptos básicos para la creación de una marca, las estrategias y habilidades que se necesitan como factor diferenciador sobre la competencia directa, para crear una marca personal y tener una buena reputación en línea. Para ello, se revisarán, los beneficios, el impacto, el marketing personal y la importancia de las redes sociales, en la marca personal o “personal branding”, así como la identidad digital y reputación en línea que es el reconocimiento que hacen los seguidores o potenciales compradores de la marca que hemos creado.".replace(/\./g, ".<br><br>"),
@@ -11,12 +11,12 @@ const productos = [
     imagen: "img/coaching.png",
     descripcion: "El coaching está presente en todos los aspectos del ser y tiene que ver con la manera en que funciona todo su sistema de coherencia (pensar, sentir, decir y hacer). Al revisarse honestamente y sin juicios, podrá explorar de qué manera le sirven o limitan sus pensamientos y acciones para crear más opciones que enriquezcan la vida, la profesión y el entorno laboral, social y de negocios. Este curso es una inmersión en el mundo del coaching, aprendiendo gran variedad de dinámicas y herramientas de calidad que propiciarán el cambio y el desarrollo personal, profesional y organizacional. El coaching ha surgido apoyándose en las neurociencias y es aplicable en un amplio campo, en el que se llevan a cabo una serie de acciones para acompañar a otras personas a conseguir sus objetivos, potenciar sus habilidades y aportar recursos para superar sus limitaciones, lograr objetivos de alto estándar y mejorar su desempeño o reenfocar sus propósitos.  ".replace(/\./g, ".<br><br>"),
     enlace: "#"
-  },
+  }, */
   {
     titulo: "Creatividad e innovación",
     imagen: "img/creatividad.png",
     descripcion: "Al realizar este curso, usted conocerá técnicas prácticas para diseñar un proyecto creativo e innovador aplicable en un entorno organizacional y/o social. El proceso inicia identificando aspectos básicos para la generación y concreción de ideas innovadoras basadas en la aplicación de la técnica “Design Thinking”. Posteriormente, se abordará la técnica SCAMPER, que brinda a los proyectos los elementos creativos necesarios para finalmente, construir una propuesta de valor única y diferencial, que tenga repercusión en el entorno. Esperamos que este curso satisfaga sus expectativas de aprendizaje y le sea de suma utilidad tanto en su contexto personal como laboral.".replace(/\./g, ".<br><br>"),
-    enlace: "	https://institucion.ucundinamarca.edu.co/course/view.php?id=255"
+    enlace: "https://institucion.ucundinamarca.edu.co/course/view.php?id=255"
   },
   {
     titulo: "Beginners English Course",
@@ -58,13 +58,13 @@ const productos = [
     titulo: "Estilos de Gerencia",
     imagen: "img/estilos.png",
     descripcion: "Con la realización de este curso y como parte del proceso de aprendizaje usted conocerá herramientas que le permitirán convertirse en un gran gerente, adaptando los estilos de gerencia al entorno organizacional en el que se encuentre, lo que le permitirá consolidar equipos y ser un líder estratégico de alto rendimiento. Los elementos trabajados en este curso le ayudarán a optimizar las relaciones interpersonales en el ambiente laboral, a través de los diferentes estilos de gerencia, utilizando la competitividad estratégica y el desarrollo humano como pilar fundamental para la solución de diversas problemáticas. Confiamos en que lograremos llenar sus expectativas con este curso y estamos seguros de que le será de suma utilidad tanto en su contexto personal como laboral.".replace(/\./g, ".<br><br>"),
-    enlace: "	https://institucion.ucundinamarca.edu.co/course/view.php?id=253"
+    enlace: "https://institucion.ucundinamarca.edu.co/course/view.php?id=253"
   },
   {
     titulo: "Pensamiento Lógico",
     imagen: "img/pensamiento.png",
     descripcion: "Con la realización de este curso y como parte del proceso, usted construirá conocimientos para generar soluciones rápidas, eficientes y metódicas a distintas situaciones problémicas o conflictos a través del uso del pensamiento lógico, basado en hechos y datos probables. Por medio del uso del pensamiento lógico, usted tendrá la oportunidad de desarrollar precisión, asertividad y exactitud, para poder entender su contexto y las relaciones presentes en él, abstraídos a partir de la observación, el análisis, la comparación, la clasificación, la experiencia, la imaginación y la creatividad. El pensamiento lógico es una herramienta que permite crear soluciones rápidas y eficientes a problemas de la vida diaria, abarcando desde lo más simple hasta lo más complejo. Confiamos en que este curso satisfaga sus expectativas de aprendizaje y le sea de suma utilidad tanto en su contexto personal como laboral.".replace(/\./g, ".<br><br>"),
-    enlace: "	https://institucion.ucundinamarca.edu.co/course/view.php?id=254"
+    enlace: "https://institucion.ucundinamarca.edu.co/course/view.php?id=254"
   },
   {
     titulo: "Análisis y Evaluación de Estrategias",
@@ -101,8 +101,32 @@ const productos = [
     imagen: "img/construccion.png",
     descripcion: "El curso Construcción y trabajo colaborativo se plantea como una experiencia formativa con el fin de que el participante valore la importancia de las prácticas, actitudes y comportamientos que configuran la cultura organizacional y de qué manera influyen en el alcance de las metas y objetivos. Las organizaciones están llamadas a configurar relaciones entre sus colaboradores que, permeadas por los principios y fundamentos, inspiren actitudes colaborativas, cooperativas y solidarias que migrarán hacia otros contextos de actuación de los sujetos vinculados a ellas, el resultado esperado de aprendizaje de este curso responde a esta necesidad creando una metodología dirigida a la creación de una comunidad colaborativa de trabajo. Esperamos en que este curso satisfaga sus expectativas de aprendizaje y le sea de suma utilidad tanto en su contexto personal como laboral.".replace(/\./g, ".<br><br>"),
     enlace: "https://institucion.ucundinamarca.edu.co/course/view.php?id=257"
+  },
+  {
+    titulo: "Liderazgo innovador",
+    imagen: "img/liderazgo.png",
+    descripcion: "Con la realización de este curso se busca coadyuvar a la construcción de conocimientos que nutran el actuar de los individuos y que ellos enriquezcan su vida profesional y laboral mediante la implementación de prácticas que propicien la innovación, las mismas que conducen, a través del ejercicio, a un liderazgo particular que permita observar en las dificultades, oportunidades y así transformar circunstancias en proyectos para la organización, que puedan ser proyectados a la comunidad en general y transformen de forma comprometida y responsable las realidades. Así pues, las instituciones del siglo XXI se enfocan en la formación de sujetos comprometidos con sus realidades inmediatas, capaces de responder a las dificultades y a la afectación que deviene de los cambios globales. El líder ejerce una influencia positiva en los otros, les incentiva y motiva. Es aquel que escucha y organiza con el fin de que se sumen esfuerzos para trabajar por un bien común. En el ambiente laboral siempre se requerirán líderes, pues se caracterizan por ser un recurso humano propositivo, talentoso y creativo. Por lo anterior, vale la pena auto conocerse e identificar las habilidades de liderazgo que cada uno posee. Esperamos que este curso satisfaga sus expectativas de aprendizaje y le sea de suma utilidad tanto en su contexto personal como laboral.".replace(/\./g, ".<br><br>"),
+    enlace: "	https://institucion.ucundinamarca.edu.co/course/view.php?id=250"
+  },
+  {
+    titulo: "Planeación, seguimiento y control",
+    imagen: "img/planeacion.png",
+    descripcion: "En este curso, usted iniciará reconociendo la importancia de la planeación en la vida cotidiana.  Se busca identificar las fortalezas, debilidades, oportunidades y amenazas del proceso, para después establecer un plan de mejoramiento individual basado en la optimización del tiempo mediante la planeación. Posteriormente, se propone la identificación y aplicación de las herramientas de la planeación en el rol o puesto de trabajo. Para ello se debe realizar un análisis de la situación inicial (prediagnóstico), así como la fijación de objetivos, metas, estrategias, tácticas, duración en tiempo, tareas, actividades y responsables, los cuales se concretan en los planes de acción respectivos. Finalmente, a partir de los planes de acción diseñados y establecidos en el rol o puesto de trabajo, se define el sistema de monitoreo o seguimiento y control. De esa manera, se identifican los métodos y enfoques de control, así como la formulación de indicadores de gestión asociados a cada una de las metas propuestas. Confiamos en que este curso satisfaga sus expectativas de aprendizaje y le sea de suma utilidad tanto en su contexto personal como laboral.".replace(/\./g, ".<br><br>"),
+    enlace: "https://institucion.ucundinamarca.edu.co/course/view.php?id=246"
+  },
+  {
+    titulo: "Comunicación personal y colectiva",
+    imagen: "img/comunicacion.png",
+    descripcion: "Con la realización del presente curso, se busca mediante la apropiación de técnicas y modelos de Comunicación Personal y Colectiva el control de lo que se transmite y un mayor desarrollo de la habilidad comunicativa. Como consecuencia, se fortalece el perfil profesional mediante una comunicación eficiente, lo que redunda en mejores resultados en el ámbito laboral. Hacer uso correcto de las palabras, considerar el significado que se les otorga, tomar consciencia del contexto y prestar atención al lenguaje no verbal, propicia una mejor comunicación con los interlocutores. Lo que se traduce, muchas veces, en una mejor interacción con el entorno social y laboral. Un mensaje claro y concreto en el entorno laboral siempre será atendido, comprendido y valorado por quien lo recibe. Propicia una respuesta objetiva y concreta, evita situaciones difíciles y genera sentimientos de apoyo por parte de la comunidad. Por lo anterior, la comunicación personal y colectiva puede ser una herramienta enriquecedora y valiosa para usted. Confiamos en que este curso satisfaga sus expectativas de aprendizaje y le sea de suma utilidad tanto en su contexto personal como laboral.".replace(/\./g, ".<br><br>"),
+    enlace: "https://institucion.ucundinamarca.edu.co/course/view.php?id=261"
+  },
+  {
+    titulo: "Administración y gestión organizacional",
+    imagen: "img/administracion.png",
+    descripcion: "Este curso se orienta a aplicar los conceptos básicos de la administración, enmarcados en las escuelas administrativas y sus diversas teorías, las cuales son fundamentales para configurar y propiciar una cultura organizacional moderna. Así mismo, el curso busca conducir a la implementación de los modelos de gestión administrativa, tanto tradicionales como prospectivos, enfocados en garantizar competitividad y sostenibilidad. Confiamos en que este curso satisfaga sus expectativas de aprendizaje y le sea de suma utilidad tanto en su contexto personal como laboral.".replace(/\./g, ".<br><br>"),
+    enlace: "https://institucion.ucundinamarca.edu.co/course/view.php?id=260"
   }
-  // Agrega más productos aquí...
+  // Agrega más cursos aquí...
 ];
 
 function mostrarProductos() {
